82e34485bafd5013412f7fb8e313bb862164cc95
Alex <alex.hoch@gmx.de>
2021-01-21 09:05:45 +0100

    Initial Commit

---
+++ SwiftUI_ToDoSaveLocal_doz_12/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_ToDoSaveLocal_doz_12
+//
+//  Created by Alexander Hoch on 21.01.21.
+//  Copyright © 2021 zancor. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
