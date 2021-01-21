d00e45a61192e56ac49794dfbc001749ce20c069
Alex <alex.hoch@gmx.de>
2021-01-21 09:14:31 +0100

    Project angelegt und StartView erzeugt

---
+++ SwiftUI_ToDoSaveLocal_doz_12/StartView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_ToDoSaveLocal_doz_12
+//
+//  Created by Alexander Hoch on 21.01.21.
+//  Copyright © 2021 zancor. All rights reserved.
+//
+
+import SwiftUI
+
+struct StartView: View {
+    var body: some View {
+        Text("The Beginning")
+    }
+}
+
+struct StartView_Previews: PreviewProvider {
+    static var previews: some View {
+        StartView()
+    }
+}
